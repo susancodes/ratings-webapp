d077c99dc90131ce333a90599945f42876df9853
Hackbright Student <info@hackbrightacademy.com>
2015-07-28 18:03:31 -0700

    working login, still working on ratings system

--- model.py
+++ model.py
@@ -15,11 +15,11 @@ db = SQLAlchemy()
 # Delete this line and put your User/Movie/Ratings model classes here.
 class User(db.Model):
     """User of ratings website."""
-    
+
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String(64), nullable=True)
+    email = db.Column(db.String(64), nullable=True, unique=True)
     password = db.Column(db.String(64), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-
+from sortedcontainers import SortedDict
 from model import User, Rating, Movie, connect_to_db, db
 
 
@@ -36,16 +36,29 @@ def handle_login():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    if email:
-        session["email"] = email
+    #If both email and password are correct, log them in
+    if User.query.filter(User.email == email, User.password == password).first():
         flash("You are now logged in.")
+        session["email"] = email
+    #if email is correct but not password, tell them they fucked up
+    elif User.query.filter(User.email == email, User.password != password).first(): 
+        flash("Incorrect login information provided. Please try again.")
+        return redirect("/login_form")
+    #If both email and pass were incorrect, make a new user
+    else:
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Welcome new user. You've been added to our database!")
+        session["email"] = email
     return redirect('/')
+
 
 @app.route('/logout', methods=["POST"])
 def handle_logout():
     """Process user logout."""
 
-    session["email"] = ""
+    del session["email"]
     flash("You are now logged out.")
     return redirect('/')
 
@@ -61,16 +74,52 @@ def show_user_info(id):
     """Show user info."""
 
     user = User.query.get(id)
-    users_movies = Rating.query.filter_by(user_id=id).all()
+    users_ratings = user.ratings
+
     movie_dict = {}
-    for movie in users_movies:
-        movie_id = movie.movie_id
-        movie_object = Movie.query.get(movie_id)
-        movie_dict[movie_object] = movie.score
+    for rating in users_ratings:
+        movie_dict[rating.movie] = rating.score
+        print rating
+
     return render_template('about_user.html', 
                             id=id,
                             age=user.age,
                             zipcode=user.zipcode,
+                            ratings=users_ratings,
+                            movie_dict=movie_dict)
+
+@app.route("/add_rating", methods=["POST"])
+def add_rating():
+    user_email = session["email"]
+    user = User.query.filter_by(email=user_email).first()
+    # NEXT THING WE HAVE TO DO IS FIND A WAY TO GET MOVIE ID IN HERE 
+    movie = Movie.query.filter_by(movie_id=)
+    score = request.form.get("score")
+
+    new_user = Rating(user_id=user.user_id, movie_id=movie_id, score=score)
+    db.session.add(new_user)
+    db.session.commit()
+
+@app.route("/movies")
+def movie_list():
+    """Show list of movies."""
+
+    movies = Movie.query.all()
+    return render_template("movie_list.html", movies=movies)
+
+@app.route("/movies/<int:id>")
+def show_movie_info(id):
+    """Show movie info."""
+
+    movie_object = Movie.query.get(id)
+    movies_ratings = movie_object.ratings
+
+    movie_dict = {}
+    for movie in movies_ratings:
+        movie_dict[movie.user] = movie.score
+
+    return render_template('about_movie.html', 
+                            movie_object=movie_object,
                             movie_dict=movie_dict)
 
 if __name__ == "__main__":
